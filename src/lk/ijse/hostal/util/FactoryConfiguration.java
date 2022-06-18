c537807cbde386a59e4f244fa3e86455c99c1f98
uwanitheekshani <uwanitheekshani@gmail.com>
2022-06-18 15:09:07 +0530

    Create layers and property configuration

---
+++ src/lk/ijse/hostal/util/FactoryConfiguration.java
@@ -0,0 +1,37 @@
+package lk.ijse.hostal.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class FactoryConfiguration {
+
+    private static FactoryConfiguration factoryConfiguration;
+    private final SessionFactory sessionFactory;
+
+
+    private FactoryConfiguration() throws IOException {
+        Configuration configuration = new Configuration();
+        Properties p = new Properties();
+        p.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("hibernate.properties"));
+        configuration.setProperties(p);
+
+        // configuration.addAnnotatedClass();
+       // configuration.addAnnotatedClass(Employee.class);
+
+        sessionFactory = configuration.buildSessionFactory();
+
+    }
+
+    public static FactoryConfiguration getInstance() throws IOException {
+        return (factoryConfiguration == null) ? factoryConfiguration = new FactoryConfiguration()
+                : factoryConfiguration;
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
+}
